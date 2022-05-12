71329441e8294abef831e63f80fa1479684af6d4
Nicolas Chartiot <nicolaschartiot@gmail.com>
2022-05-12 18:04:22 -0300

    WIP: Experiment with grid system and align pixels

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,38 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>();
-  const [currentColor, setCurrentColor] = useState('red');
-  const [artSize, setArtSize] = useState({ width: 16, height: 16 });
+  const [artSize, setArtSize] = useState({ width: 40, height: 16 });
 
-  // const resizeCanvas = () => {
-  //   canvas.style.height = `90vh`;
-  //   canvas.style.width = `90vw`;
-  // };
+  const pixelSize = Math.round(1000 / 40);
+
+  let mousePositionX: Number;
+  let mousePositionY: Number;
+
+  // Returns the coordinate on the pixel grid (e.g. [1, 1] or [15, 4])
+  const getCanvasCoordinate = (x, y) => {
+    const canvasX = Math.ceil(x / pixelSize);
+    const canvasY = Math.ceil(y / pixelSize);
+
+    return [canvasX, canvasY];
+  };
+
+  // Returns the pixel's [0, 0] on the canvas (e.g. [63, 63] or [256, 144])
+  const getPixelCoordinate = (x, y) => {
+    const pixelX = pixelSize * x - pixelSize;
+    const pixelY = pixelSize * y - pixelSize;
+
+    return [pixelX, pixelY];
+  };
+
+  const paintPixel = (x, y, color) => {
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = color;
+
+    const [fillX, fillY] = getPixelCoordinate(x, y);
+    console.log(fillX, fillY);
+    ctx.fillRect(fillX, fillY, pixelSize, pixelSize);
+  };
 
   const setupEventListeners = () => {
     canvas.addEventListener('mousedown', (e) => {
@@ -20,81 +45,67 @@ export default function Home() {
       const y = e.clientY - rect.top;
 
       ctx.fillStyle = 'black';
-      ctx.fillRect(x, y, ctx.canvas.width / 16, ctx.canvas.height / 16);
+
+      const [canvasX, canvasY] = getCanvasCoordinate(x, y);
+      console.log(canvasX, canvasY);
+
+      paintPixel(canvasX, canvasY, 'black');
     });
 
-    canvas.addEventListener('mousemove', () => drawInfiniteGrid());
+    canvas.addEventListener('mousemove', (e) => {
+      const ctx = canvas.getContext('2d');
+      const rect = canvas.getBoundingClientRect();
 
-    // canvas.addEventListener('mousemove', (e) => {
-    //   const ctx = canvas.getContext('2d');
-    //   const rect = canvas.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
 
-    //   const x = e.clientX - rect.left;
-    //   const y = e.clientY - rect.top;
-
-    //   ctx.fillStyle = 'black';
-    //   ctx.fillRect(x, y, artSize.width, artSize.height);
-    // });
+      const [mousePositionX, mousePositionY] = getCanvasCoordinate(x, y);
+    });
   };
 
   const drawGrid = () => {
-    // const
-  };
-
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  const drawInfiniteGrid = async () => {
     const ctx = canvas.getContext('2d');
-    const colors = ['red', 'green', 'blue', 'yellow', 'black', 'purple', 'white'];
 
-    for (let k = 0; k < 1; k++) {
-      for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-          ctx.fillStyle = colors[Math.floor(Math.random() * 7)];
-          ctx.fillRect(i * 62.5, j * 62.5, 62.5, 62.5);
+    const cw = canvas.width;
+    const ch = canvas.height;
+
+    for (let i = 0; i < ch; i += pixelSize) {
+      for (let j = 0; j < cw; j += pixelSize) {
+        if (i % 2 === 1 && j % 2 === 0) {
+          const [canvasX, canvasY] = getCanvasCoordinate(i, j);
+          paintPixel(canvasX, canvasY, 'lightGrey');
+        }
+        if (i % 2 === 0 && j % 2 === 1) {
+          const [canvasX, canvasY] = getCanvasCoordinate(i, j);
+          paintPixel(canvasX, canvasY, 'lightGrey');
         }
       }
     }
   };
 
+  const resizeCanvas = (ctx) => {
+    const bodyWidth = document.body.clientWidth;
+    const bodyHeight = document.body.clientHeight;
+
+    const canvasPixelSize = Math.floor(bodyWidth / artSize.width);
+
+    const canvasWidth = canvasPixelSize * artSize.width;
+    const canvasHeight = canvasPixelSize * artSize.height;
+
+    ctx.canvas.width = canvasWidth;
+    ctx.canvas.height = canvasHeight;
+  };
+
   useEffect(() => {
-    const countFps = () => {
-      const kFps: HTMLHeadingElement = document.querySelector('#kFps');
-      const kpFps: HTMLProgressElement = document.querySelector('#kpFps');
-
-      let be = Date.now(),
-        fps = 0;
-      requestAnimationFrame(function loop() {
-        let now = Date.now();
-        fps = Math.round(1000 / (now - be));
-        be = now;
-        requestAnimationFrame(loop);
-        if (fps < 35) {
-          kFps.style.color = 'red';
-          kFps.textContent = `${fps}`;
-        }
-        if (fps >= 35 && fps <= 41) {
-          kFps.style.color = 'deepskyblue';
-          kFps.textContent = fps + ' FPS';
-        } else {
-          kFps.style.color = 'white';
-          kFps.textContent = fps + ' FPS';
-        }
-        kpFps.value = fps;
-      });
-    };
-
-    countFps();
-
     if (canvas) {
       setupEventListeners();
       // Context for the canvas for 2 dimensional operations
       const ctx = canvas.getContext('2d');
 
-      ctx.canvas.width = 1000;
-      ctx.canvas.height = 1000;
+      // ctx.canvas.width = 1000;
+      // ctx.canvas.height = 1000;
+
+      resizeCanvas(ctx);
 
       const width = canvas.offsetWidth;
       const height = canvas.offsetHeight;
@@ -104,6 +115,8 @@ export default function Home() {
       ctx.fillRect(0, 0, width, height);
 
       ctx.lineTo;
+
+      drawGrid();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [canvas]);
@@ -116,11 +129,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <button onClick={drawInfiniteGrid}>Start show</button>
-      <h3 id="kFps" className="text-white">
-        0
-      </h3>
-      <progress id="kpFps" value="0" max="100"></progress>
       <main className="p-14 text-white">
         <canvas ref={(node) => setCanvas(node)} />
       </main>
