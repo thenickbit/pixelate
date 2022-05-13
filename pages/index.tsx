b8492201be7e357210354fdcc40e7d5178d5936e
Nicolas Chartiot <nicolaschartiot@gmail.com>
2022-05-13 19:58:35 -0300

    Draw and resize canvas grid based on art size

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,35 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>();
-  const [artSize, setArtSize] = useState({ width: 40, height: 16 });
-
-  const pixelSize = Math.round(1000 / 40);
-
-  let mousePositionX: Number;
-  let mousePositionY: Number;
+  const [artSize, setArtSize] = useState({ width: 15, height: 15 });
+  const pixelSize = useRef(0);
 
   // Returns the coordinate on the pixel grid (e.g. [1, 1] or [15, 4])
   const getCanvasCoordinate = (x, y) => {
-    const canvasX = Math.ceil(x / pixelSize);
-    const canvasY = Math.ceil(y / pixelSize);
+    const canvasX = Math.ceil(x / pixelSize.current);
+    const canvasY = Math.ceil(y / pixelSize.current);
 
     return [canvasX, canvasY];
   };
 
   // Returns the pixel's [0, 0] on the canvas (e.g. [63, 63] or [256, 144])
   const getPixelCoordinate = (x, y) => {
-    const pixelX = pixelSize * x - pixelSize;
-    const pixelY = pixelSize * y - pixelSize;
+    const pixelX = pixelSize.current * x - pixelSize.current;
+    const pixelY = pixelSize.current * y - pixelSize.current;
 
     return [pixelX, pixelY];
   };
 
+  // Paints a pixel based on getCanvasCoordinate x and y values
   const paintPixel = (x, y, color) => {
     const ctx = canvas.getContext('2d');
 
     ctx.fillStyle = color;
 
     const [fillX, fillY] = getPixelCoordinate(x, y);
-    console.log(fillX, fillY);
-    ctx.fillRect(fillX, fillY, pixelSize, pixelSize);
+    ctx.fillRect(fillX, fillY, pixelSize.current, pixelSize.current);
   };
 
   const setupEventListeners = () => {
@@ -47,7 +43,6 @@ export default function Home() {
       ctx.fillStyle = 'black';
 
       const [canvasX, canvasY] = getCanvasCoordinate(x, y);
-      console.log(canvasX, canvasY);
 
       paintPixel(canvasX, canvasY, 'black');
     });
@@ -64,46 +59,45 @@ export default function Home() {
   };
 
   const drawGrid = () => {
-    const ctx = canvas.getContext('2d');
-
-    const cw = canvas.width;
-    const ch = canvas.height;
-
-    for (let i = 0; i < ch; i += pixelSize) {
-      for (let j = 0; j < cw; j += pixelSize) {
-        if (i % 2 === 1 && j % 2 === 0) {
-          const [canvasX, canvasY] = getCanvasCoordinate(i, j);
-          paintPixel(canvasX, canvasY, 'lightGrey');
-        }
-        if (i % 2 === 0 && j % 2 === 1) {
-          const [canvasX, canvasY] = getCanvasCoordinate(i, j);
-          paintPixel(canvasX, canvasY, 'lightGrey');
+    for (let h = 1; h <= artSize.height; h++) {
+      for (let w = 1; w <= artSize.width; w++) {
+        if (h % 2 === 1 && w % 2 === 0) {
+          paintPixel(w, h, 'lightGrey');
+        } else if (h % 2 === 0 && w % 2 === 1) {
+          paintPixel(w, h, 'lightGrey');
+        } else {
+          paintPixel(w, h, 'white');
         }
       }
     }
   };
 
-  const resizeCanvas = (ctx) => {
+  const getPixelSize = () => {
     const bodyWidth = document.body.clientWidth;
     const bodyHeight = document.body.clientHeight;
 
-    const canvasPixelSize = Math.floor(bodyWidth / artSize.width);
+    const pixelWidth = bodyWidth / artSize.width;
+    const pixelHeight = bodyHeight / artSize.height;
 
-    const canvasWidth = canvasPixelSize * artSize.width;
-    const canvasHeight = canvasPixelSize * artSize.height;
+    return Math.round(Math.min(pixelHeight, pixelWidth));
+  };
+
+  const resizeCanvas = (ctx) => {
+    const canvasWidth = pixelSize.current * artSize.width;
+    const canvasHeight = pixelSize.current * artSize.height;
 
     ctx.canvas.width = canvasWidth;
     ctx.canvas.height = canvasHeight;
   };
 
   useEffect(() => {
+    const size = getPixelSize();
+    pixelSize.current = size;
+
     if (canvas) {
       setupEventListeners();
       // Context for the canvas for 2 dimensional operations
       const ctx = canvas.getContext('2d');
-
-      // ctx.canvas.width = 1000;
-      // ctx.canvas.height = 1000;
 
       resizeCanvas(ctx);
 
@@ -113,8 +107,6 @@ export default function Home() {
       ctx.fillStyle = 'white';
       ctx.globalAlpha = 1;
       ctx.fillRect(0, 0, width, height);
-
-      ctx.lineTo;
 
       drawGrid();
     }
